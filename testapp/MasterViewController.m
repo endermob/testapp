1e259071014a30be8b947832626a951e60aa8853
enderozcan <Ozcan@Enders-MacBook-Pro.local>
2014-05-21 18:00:20 +0300

    initial commit
    test app

--- testapp/MasterViewController.m
+++ testapp/MasterViewController.m
@@ -1,7 +1,7 @@
 //
 //  MasterViewController.m
 //  testapp
-//
+//  Ender Özcan
 //  Created by Ender on 21/05/14.
 //  Copyright (c) 2014 mobilike. All rights reserved.
 //
@@ -14,6 +14,7 @@
     NSMutableArray *_objects;
 }
 @end
+
 
 @implementation MasterViewController
 
